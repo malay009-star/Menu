7b3a134bf47540cb7946f01e494dcf0ebdc86fe9
malay009-star <117511789+malay009-star@users.noreply.github.com>
2023-05-30 12:27:00 +0500

    we rechange all project

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,51 +1,350 @@
-let all_btn = document.getElementById('all_btn');
+var All_item = document.getElementById('item_box');
 
-let breakfast_btn = document.getElementById('breakfast_btn');
-
-let lunch_btn = document.getElementById('lunch_btn');
-
-let shakes_btn = document.getElementById('shakes_btn');
-
-let dinner_btn = document.getElementById('dinner_btn');
-
-// Content 
-var All = document.getElementById('All');
-var BreakFast = document.getElementById('Breakfast');
-var Lunch = document.getElementById('Lunch');
-var Shakes = document.getElementById('Shakes');
-var Dinner = document.getElementById('Dinner');
+console.log(All_item);
 
 
-// control html
-all_btn.addEventListener('click', () => {
-    All.style.display = 'block';
-    BreakFast.style.display = 'block';
-    Lunch.style.display = 'block';
-    Shakes.style.display = 'block';
-    Dinner.style.display = 'block';
-});
-breakfast_btn.addEventListener('click', () => {
-    BreakFast.style.display = 'block';
-    Lunch.style.display = 'none';
-    Shakes.style.display = 'none';
-    Dinner.style.display = 'none';
-});
-lunch_btn.addEventListener('click', () => {
-    Lunch.style.display = 'block';
-    BreakFast.style.display = 'none';
-    Shakes.style.display = 'none';
-    Dinner.style.display = 'none';
-});
-shakes_btn.addEventListener('click', () => {
-    Shakes.style.display = 'block';
-    Lunch.style.display = 'none';
-    BreakFast.style.display = 'none';
-    Dinner.style.display = 'none';
-});
-dinner_btn.addEventListener('click', () => {
-    Dinner.style.display = 'block';
-    Shakes.style.display = 'none';
-    Lunch.style.display = 'none';
-    BreakFast.style.display = 'none';
+var allObjects = [{
+        img: 'Assets/img/item-1.jpeg',
+        title: 'Buttermilk Pancakes',
+        price: '$15.99',
+        type: 'Breakfast',
+        description: 'I m baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed'
+    },
+    {
+        img: 'Assets/img/item-3.jpeg',
+        title: 'Bacon Overflow',
+        price: '$15.99',
+        type: 'Breakfast',
+        description: 'I m baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed'
+    },
+    {
+        img: 'Assets/img/item-4.jpeg',
+        title: 'Diner Double',
+        price: '$15.99',
+        type: 'Lunch',
+        description: 'I m baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed'
+    },
+    {
+        img: 'Assets/img/item-5.jpeg',
+        title: 'Egg Attack',
+        price: '$15.99',
+        type: 'Lunch',
+        description: 'I m baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed'
+    },
+    {
+        img: 'Assets/img/item-8.jpeg',
+        title: 'Oreo Dream',
+        price: '$15.99',
+        type: 'Shakes',
+        description: 'I m baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed'
+    },
+    {
+        img: 'Assets/img/item-9.jpeg',
+        title: 'Quarantine Buddy',
+        price: '$15.99',
+        type: 'Shakes',
+        description: 'I m baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed'
+    },
+    {
+        img: 'Assets/img/item-2.jpeg',
+        title: 'Country Delight',
+        price: '$15.99',
+        type: 'Breakfast',
+        description: 'I m baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed'
+    },
 
-});
+    {
+        img: 'Assets/img/item-10.jpeg',
+        title: 'Steak Dinner',
+        price: '$15.99',
+        type: 'Dinner',
+        description: 'I m baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed'
+    },
+    {
+        img: 'Assets/img/item-1.jpeg',
+        title: 'Country Delight',
+        price: '$15.99',
+        type: 'Dinner',
+        description: 'I m baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed'
+    },
+
+    {
+        img: 'Assets/img/item-6.jpeg',
+        title: 'Steak Dinner',
+        price: '$15.99',
+        type: 'Lunch',
+        description: 'I m baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed'
+    }
+]
+
+// by default content
+allObjects.forEach(item => {
+    const { img, title, price, description, type } = item;
+    let x = document.createElement('div');
+    let y = document.createElement('div');
+    let z = document.createElement('div');
+
+    // main div of item info
+    let final_div = document.createElement('div');
+    final_div.classList.add('w-1/4');
+    final_div.classList.add('px-2');
+    final_div.classList.add('pb-14');
+
+    // div for image
+    x.classList.add('w-1/4');
+    let food_img = `
+    <img src="${img}"/>`
+    x.innerHTML = food_img;
+    All_item.appendChild(x);
+
+    // div for title and price
+    y.classList.add('flex');
+    y.classList.add('justify-between');
+    let titl_pric = ` 
+    <h2>${title}</h2>
+    <pre>${price}</pre>`
+    y.innerHTML = titl_pric;
+    final_div.appendChild(y);
+
+    // div for description
+    z.classList.add('pb-14');
+    let my_data = `
+    <p>${description}</p>`
+    z.innerHTML = my_data
+    final_div.appendChild(z);
+    // let text = document.createTextNode(my_data)
+
+    All_item.appendChild(final_div)
+})
+
+
+// oncliking all
+document.getElementById('all_btn').addEventListener('click', () => {
+    All_item.innerHTML = '';
+
+    allObjects.forEach(item => {
+            const { img, title, price, description, type } = item;
+            let x = document.createElement('div');
+            let y = document.createElement('div');
+            let z = document.createElement('div');
+
+            // main div of item info
+            let final_div = document.createElement('div');
+            final_div.classList.add('w-1/4');
+            final_div.classList.add('px-2');
+            final_div.classList.add('pb-14');
+
+            // div for image
+            x.classList.add('w-1/4');
+            let food_img = `
+        <img src="${img}"/>`
+            x.innerHTML = food_img;
+            All_item.appendChild(x);
+
+            // div for title and price
+            y.classList.add('flex');
+            y.classList.add('justify-between');
+            let titl_pric = ` 
+        <h2>${title}</h2>
+        <pre>${price}</pre>`
+            y.innerHTML = titl_pric;
+            final_div.appendChild(y);
+
+            // div for description
+            z.classList.add('pb-14');
+            let my_data = `
+        <p>${description}</p>`
+            z.innerHTML = my_data
+            final_div.appendChild(z);
+            // let text = document.createTextNode(my_data)
+
+            All_item.appendChild(final_div)
+        })
+        // }
+})
+
+
+// break fast
+document.querySelector('#breakfast_btn').addEventListener('click', ({ target: { innerHTML } }) => {
+    All_item.innerHTML = '';
+    const value = innerHTML;
+    const x = allObjects.filter(item => item.type === value)
+    x.forEach(item => {
+        const { img, title, price, description, type } = item;
+        let x = document.createElement('div');
+        let y = document.createElement('div');
+        let z = document.createElement('div');
+
+        // main div of item info
+        let final_div = document.createElement('div');
+        final_div.classList.add('w-1/4');
+        final_div.classList.add('px-2');
+        final_div.classList.add('pb-14');
+
+        // div for image
+        x.classList.add('w-1/4');
+        let food_img = `
+        <img src="${img}"/>`
+        x.innerHTML = food_img;
+        All_item.appendChild(x);
+
+        // div for title and price
+        y.classList.add('flex');
+        y.classList.add('justify-between');
+        let titl_pric = ` 
+        <h2>${title}</h2>
+        <pre>${price}</pre>`
+        y.innerHTML = titl_pric;
+        final_div.appendChild(y);
+
+        // div for description
+        z.classList.add('pb-14');
+        let my_data = `
+        <p>${description}</p>`
+        z.innerHTML = my_data
+        final_div.appendChild(z);
+        // let text = document.createTextNode(my_data)
+
+        All_item.appendChild(final_div)
+    })
+})
+
+
+// lunch
+document.querySelector('#lunch_btn').addEventListener('click', ({ target: { innerHTML } }) => {
+    All_item.innerHTML = '';
+    const value = innerHTML;
+    const x = allObjects.filter(item => item.type === value)
+    x.forEach(item => {
+        const { img, title, price, description, type } = item;
+        let x = document.createElement('div');
+        let y = document.createElement('div');
+        let z = document.createElement('div');
+
+        // main div of item info
+        let final_div = document.createElement('div');
+        final_div.classList.add('w-1/4');
+        final_div.classList.add('px-2');
+        final_div.classList.add('pb-14');
+
+        // div for image
+        x.classList.add('w-1/4');
+        let food_img = `
+        <img src="${img}"/>`
+        x.innerHTML = food_img;
+        All_item.appendChild(x);
+
+        // div for title and price
+        y.classList.add('flex');
+        y.classList.add('justify-between');
+        let titl_pric = ` 
+        <h2>${title}</h2>
+        <pre>${price}</pre>`
+        y.innerHTML = titl_pric;
+        final_div.appendChild(y);
+
+        // div for description
+        z.classList.add('pb-14');
+        let my_data = `
+        <p>${description}</p>`
+        z.innerHTML = my_data
+        final_div.appendChild(z);
+        // let text = document.createTextNode(my_data)
+
+        All_item.appendChild(final_div)
+    })
+})
+
+
+// Shakes
+document.querySelector('#shakes_btn').addEventListener('click', ({ target: { innerHTML } }) => {
+    All_item.innerHTML = '';
+    const value = innerHTML;
+    const x = allObjects.filter(item => item.type === value)
+    x.forEach(item => {
+        const { img, title, price, description, type } = item;
+        let x = document.createElement('div');
+        let y = document.createElement('div');
+        let z = document.createElement('div');
+
+        // main div of item info
+        let final_div = document.createElement('div');
+        final_div.classList.add('w-1/4');
+        final_div.classList.add('px-2');
+        final_div.classList.add('pb-14');
+
+        // div for image
+        x.classList.add('w-1/4');
+        let food_img = `
+        <img src="${img}"/>`
+        x.innerHTML = food_img;
+        All_item.appendChild(x);
+
+        // div for title and price
+        y.classList.add('flex');
+        y.classList.add('justify-between');
+        let titl_pric = ` 
+        <h2>${title}</h2>
+        <pre>${price}</pre>`
+        y.innerHTML = titl_pric;
+        final_div.appendChild(y);
+
+        // div for description
+        z.classList.add('pb-14');
+        let my_data = `
+        <p>${description}</p>`
+        z.innerHTML = my_data
+        final_div.appendChild(z);
+        // let text = document.createTextNode(my_data)
+
+        All_item.appendChild(final_div)
+    })
+})
+
+
+// Dinner
+document.getElementById('dinner_btn').addEventListener('click', ({ target: { innerHTML } }) => {
+    All_item.innerHTML = '';
+    const value = innerHTML;
+    const x = allObjects.filter(item => item.type === value);
+
+    x.forEach(item => {
+        const { img, title, price, description, type } = item;
+        let x = document.createElement('div');
+        let y = document.createElement('div');
+        let z = document.createElement('div');
+
+        // main div of item info
+        let final_div = document.createElement('div');
+        final_div.classList.add('w-1/4');
+        final_div.classList.add('px-2');
+        final_div.classList.add('pb-14');
+
+        // div for image
+        x.classList.add('w-1/4');
+        let food_img = `
+        <img src="${img}"/>`
+        x.innerHTML = food_img;
+        All_item.appendChild(x);
+
+        // div for title and price
+        y.classList.add('flex');
+        y.classList.add('justify-between');
+        let titl_pric = ` 
+        <h2>${title}</h2>
+        <pre>${price}</pre>`
+        y.innerHTML = titl_pric;
+        final_div.appendChild(y);
+
+        // div for description
+        z.classList.add('pb-14');
+        let my_data = `
+        <p>${description}</p>`
+        z.innerHTML = my_data
+        final_div.appendChild(z);
+        // let text = document.createTextNode(my_data)
+
+        All_item.appendChild(final_div)
+    })
+
+})
